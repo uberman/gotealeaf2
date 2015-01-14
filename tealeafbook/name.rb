69b318ad4f12c4d444162d6325b6dad070191a9d
Manny R <elroldan@gmail.com>
2015-01-13 19:01:11 -0800

    moving files

---
+++ tealeafbook/name.rb
@@ -0,0 +1,3 @@
+puts "what is your name?"
+name=gets.chomp.capitalize
+10.times {puts "hello #{name}!"}
---
+++ tealeafbook/string_catenator.rb
@@ -0,0 +1,7 @@
+puts "What is your first name?"
+first_name=gets.chomp
+
+puts "What is your last name?"
+last_name=gets.chomp
+
+puts "#{first_name} #{last_name}"
