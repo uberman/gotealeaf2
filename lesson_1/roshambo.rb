a99a9fad259b0d89a7fea4d1cc5d5042f73dff96
Uber Man <elroldan@gmail.com>
2015-01-25 12:58:05 -0800

    cleanup roshambo first version

--- lesson_1/roshambo.rb
+++ lesson_1/roshambo.rb
@@ -19,26 +19,26 @@ def choose_weapons(num)
     end
 end
 
-def battle(player_weapon, player2)
+def battle(player_weapon, computer_weapon)
   player_name = @player_name
   case 
-  when player_weapon == 0 && player2 == 0
+  when player_weapon == 0 && computer_weapon == 0
     say("Rock vs rock! Tie!")
-  when player_weapon == 0 && player2 == 1
+  when player_weapon == 0 && computer_weapon == 1
     say("Computer's paper beats #{player_name}'s rock")
-  when player_weapon == 0 && player2 == 2
+  when player_weapon == 0 && computer_weapon == 2
     say("#{player_name}'s rock beats Computer's scissors")
-  when player_weapon == 1 && player2 == 0
+  when player_weapon == 1 && computer_weapon == 0
     say("#{player_name}'s paper beat's Computer's rock")
-  when player_weapon == 1 && player2 == 1
+  when player_weapon == 1 && computer_weapon == 1
     say("Paper vs paper! Tie!")
-  when player_weapon == 1 && player2 == 2
+  when player_weapon == 1 && computer_weapon == 2
     say("Computer's scissors beats #{player_name}'s paper")
-  when player_weapon == 2 && player2 == 0
+  when player_weapon == 2 && computer_weapon == 0
     say("Computer's rock beats #{player_name}'s scissors")
-  when player_weapon == 2 && player2 == 1
+  when player_weapon == 2 && computer_weapon == 1
     say("#{player_name}'s scissors beats Computer's paper")
-  when player_weapon == 2 && player2 == 2
+  when player_weapon == 2 && computer_weapon == 2
     say("Scissors vs scissors! Tie!")
   end
 end
@@ -48,17 +48,18 @@ say("Welcome to Ruby Roshambo! What's your name?")
 say("Hey #{@player_name}, let's play Ro Sham Bo!")
 
 begin
-  say("Choose your weapon! (r/p/s)")
-  weapon = gets.chomp.downcase
+
+  begin
+    say("Choose your weapon! (r/p/s)")
+    weapon = gets.chomp.downcase
+  end until weapon == "r" || weapon == "p" || weapon == "s"
 
   if weapon == "r"
-   player_weapon = 0
+    player_weapon = 0
   elsif weapon ==  "p" 
     player_weapon = 1
   elsif weapon == "s"
     player_weapon = 2
-  else
-    puts "#{@player_name}, pick rock, paper or scissors."
   end
 
   computer_weapon = (rand(0..2))
